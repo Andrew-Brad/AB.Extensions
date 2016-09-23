9be58847c2fe30c7f35cc5dc2ad24ba4b6138be0
Andrew <Andrew@DESKTOP-13BTVGN>
2016-09-22 20:03:33 -0500

    A few MoreLinq extensions added from https://github.com/morelinq/MoreLINQ.
    Didn't add unit tests because these are mostly self-evident, but if there are any issues I can always refer back to the repo and grab them.

---
+++ src/AB.Extensions/Common.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AB.Extensions
+{
+    public static class Common
+    {
+        /// <summary>
+        /// Enumeration that defines values representing valid ordering directions for a sequence.
+        /// </summary>
+        public enum OrderByDirection
+        {
+            /// <summary>
+            /// Elements are ordered by increasing value
+            /// </summary>
+            Ascending = 0,
+            /// <summary>
+            /// Elements are ordered by decreasing value
+            /// </summary>
+            Descending = 1,
+        }
+
+        /// <summary>
+        /// Enumeration that defines the four major cardinal directions.
+        /// </summary>
+        public enum CardinalDirection
+        {
+            Unspecified = 0,
+            North = 1,
+            South = 2,
+            East = 3,
+            West = 4
+        }
+    }
+}
---
+++ src/AB.Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static AB.Extensions.Common;
+
+namespace AB.Extensions
+{
+    /// <summary>
+    /// Credit goes to Jon Skeet and Atif Aziz at https://github.com/morelinq/MoreLINQ for these.  BRILLIANT.
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Sorts the elements of a sequence in a particular direction (ascending, descending) according to a key
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the source sequence</typeparam>
+        /// <typeparam name="TKey">The type of the key used to order elements</typeparam>
+        /// <param name="source">The sequence to order</param>
+        /// <param name="keySelector">A key selector function</param>
+        /// <param name="direction">A direction in which to order the elements (ascending, descending)</param>
+        /// <returns>An ordered copy of the source sequence</returns>
+
+        public static IOrderedEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector, OrderByDirection direction)
+        {
+            return OrderBy(source, keySelector, null, direction);
+        }
+
+        /// <summary>
+        /// Sorts the elements of a sequence in a particular direction (ascending, descending) according to a key
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the source sequence</typeparam>
+        /// <typeparam name="TKey">The type of the key used to order elements</typeparam>
+        /// <param name="source">The sequence to order</param>
+        /// <param name="keySelector">A key selector function</param>
+        /// <param name="direction">A direction in which to order the elements (ascending, descending)</param>
+        /// <param name="comparer">A comparer used to define the semantics of element comparison</param>
+        /// <returns>An ordered copy of the source sequence</returns>
+
+        public static IOrderedEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector, IComparer<TKey> comparer, OrderByDirection direction)
+        {
+            return direction == OrderByDirection.Ascending
+                       ? source.OrderBy(keySelector, comparer)
+                       : source.OrderByDescending(keySelector, comparer);
+        }
+
+        /// <summary>
+        /// Performs a subsequent ordering of elements in a sequence in a particular direction (ascending, descending) according to a key
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the source sequence</typeparam>
+        /// <typeparam name="TKey">The type of the key used to order elements</typeparam>
+        /// <param name="source">The sequence to order</param>
+        /// <param name="keySelector">A key selector function</param>
+        /// <param name="direction">A direction in which to order the elements (ascending, descending)</param>
+        /// <returns>An ordered copy of the source sequence</returns>
+
+        public static IOrderedEnumerable<T> ThenBy<T, TKey>(this IOrderedEnumerable<T> source, Func<T, TKey> keySelector, OrderByDirection direction)
+        {
+            return ThenBy(source, keySelector, null, direction);
+        }
+
+        /// <summary>
+        /// Performs a subsequent ordering of elements in a sequence in a particular direction (ascending, descending) according to a key
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the source sequence</typeparam>
+        /// <typeparam name="TKey">The type of the key used to order elements</typeparam>
+        /// <param name="source">The sequence to order</param>
+        /// <param name="keySelector">A key selector function</param>
+        /// <param name="direction">A direction in which to order the elements (ascending, descending)</param>
+        /// <param name="comparer">A comparer used to define the semantics of element comparison</param>
+        /// <returns>An ordered copy of the source sequence</returns>
+
+        public static IOrderedEnumerable<T> ThenBy<T, TKey>(this IOrderedEnumerable<T> source, Func<T, TKey> keySelector, IComparer<TKey> comparer, OrderByDirection direction)
+        {
+            return direction == OrderByDirection.Ascending
+                       ? source.ThenBy(keySelector, comparer)
+                       : source.ThenByDescending(keySelector, comparer);
+        }
+
+        /// <summary>
+        /// Skips items from the input sequence until the given predicate returns true
+        /// when applied to the current source item; that item will be the last skipped.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// SkipUntil differs from Enumerable.SkipWhile in two respects. Firstly, the sense
+        /// of the predicate is reversed: it is expected that the predicate will return false
+        /// to start with, and then return true - for example, when trying to find a matching
+        /// item in a sequence.
+        /// </para>
+        /// <para>
+        /// Secondly, SkipUntil skips the element which causes the predicate to return true. For
+        /// example, in a sequence <code>{ 1, 2, 3, 4, 5 }</code> and with a predicate of
+        /// <code>x => x == 3</code>, the result would be <code>{ 4, 5 }</code>.
+        /// </para>
+        /// <para>
+        /// SkipUntil is as lazy as possible: it will not iterate over the source sequence
+        /// until it has to, it won't iterate further than it has to, and it won't evaluate
+        /// the predicate until it has to. (This means that an item may be returned which would
+        /// actually cause the predicate to throw an exception if it were evaluated, so long as
+        /// it comes after the first item causing the predicate to return true.)
+        /// </para>
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="predicate">Predicate used to determine when to stop yielding results from the source.</param>
+        /// <returns>Items from the source sequence after the predicate first returns true when applied to the item.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="predicate"/> is null</exception>
+
+        public static IEnumerable<TSource> SkipUntil<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+            return SkipUntilImpl(source, predicate);
+        }
+
+        private static IEnumerable<TSource> SkipUntilImpl<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            using (var iterator = source.GetEnumerator())
+            {
+                while (iterator.MoveNext())
+                {
+                    if (predicate(iterator.Current))
+                    {
+                        break;
+                    }
+                }
+                while (iterator.MoveNext())
+                {
+                    yield return iterator.Current;
+                }
+            }
+        }
+        /// <summary>
+        /// Returns items from the input sequence until the given predicate returns true
+        /// when applied to the current source item; that item will be the last returned.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// TakeUntil differs from Enumerable.TakeWhile in two respects. Firstly, the sense
+        /// of the predicate is reversed: it is expected that the predicate will return false
+        /// to start with, and then return true - for example, when trying to find a matching
+        /// item in a sequence.
+        /// </para>
+        /// <para>
+        /// Secondly, TakeUntil yields the element which causes the predicate to return true. For
+        /// example, in a sequence <code>{ 1, 2, 3, 4, 5 }</code> and with a predicate of
+        /// <code>x => x == 3</code>, the result would be <code>{ 1, 2, 3 }</code>.
+        /// </para>
+        /// <para>
+        /// TakeUntil is as lazy as possible: it will not iterate over the source sequence
+        /// until it has to, it won't iterate further than it has to, and it won't evaluate
+        /// the predicate until it has to. (This means that an item may be returned which would
+        /// actually cause the predicate to throw an exception if it were evaluated, so long as
+        /// no more items of data are requested.)
+        /// </para>
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="predicate">Predicate used to determine when to stop yielding results from the source.</param>
+        /// <returns>Items from the source sequence, until the predicate returns true when applied to the item.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="predicate"/> is null</exception>
+
+        public static IEnumerable<TSource> TakeUntil<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+            return TakeUntilImpl(source, predicate);
+        }
+
+        private static IEnumerable<TSource> TakeUntilImpl<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                yield return item;
+                if (predicate(item))
+                {
+                    yield break;
+                }
+            }
+        }
+    }
+}
