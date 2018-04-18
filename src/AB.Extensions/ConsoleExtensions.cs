90717907959cd4ec04f0faf6872a079a962ea65e
Andrew <zep519@yahoo.com>
2018-04-17 20:16:55 -0500

    added Core 2.0 console project and some static extension methods to print text to console

--- src/AB.Extensions/ConsoleExtensions.cs
+++ src/AB.Extensions/ConsoleExtensions.cs
@@ -1,17 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AB.Extensions
 {
     public class ConsoleExtensions
     {
+        /// <summary>
+        /// A simple <see cref="Console.WriteLine"/> but with a foreground color toggle to keep your code clean.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
         public static void WriteLineWithColor(string message, ConsoleColor color)
         {
             Console.ForegroundColor = color;
             Console.WriteLine(message);
             Console.ResetColor();
+        }
+
+        /// <summary>
+        /// A simple <see cref="Console.Write"/> but with a foreground color toggle to keep your code clean.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        public static void WriteWithColor(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// A wrapper function that writes to the bottom line of your console, resetting the cursor to its original position.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        public static void WriteToBottomLineWithColor(string message, ConsoleColor color = ConsoleColor.Gray)
+        {
+            //Capture existing cursor position:
+            int cursorLeft = Console.CursorLeft;
+            int cursorTop = Console.CursorTop;
+            //Set cursor to bottom line:
+            Console.CursorLeft = 0;
+            Console.CursorTop = Console.WindowHeight - 1;
+            //Print Line:
+            WriteWithColor(message, color);
+            //Reset to original position:
+            Console.SetCursorPosition(cursorLeft, cursorTop);
         }
     }
 }
---
+++ test/CoreConsoleApp20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using static AB.Extensions.ConsoleExtensions;
+
+namespace CoreConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLineWithColor("Printing in yellow.", ConsoleColor.Yellow);
+            WriteLineWithColor("Cursor Left: " + Console.CursorLeft + ".  Cursor top:" + Console.CursorTop, ConsoleColor.White);
+            WriteToBottomLineWithColor("Setting time...", ConsoleColor.Red);
+            
+            //Infinite task to print time at the bottom:
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(1000);
+                    WriteToBottomLineWithColor(DateTime.Now.ToLongTimeString(), ConsoleColor.DarkRed);
+                }
+            });
+            Console.ReadKey();
+        }
+
+    }
+}
