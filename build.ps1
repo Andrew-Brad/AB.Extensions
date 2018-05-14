f8338ee101a2818694ceb0ab4dd27c2679550a0f
Andrew <zep519@yahoo.com>
2018-05-13 20:51:11 -0500

    still playing with build script

--- build.ps1
+++ build.ps1
@@ -59,6 +59,6 @@ Invoke-MSBuild
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet test .\test\MediatR.Tests -c Release }
+exec { & dotnet test .\test\AB.Extensions.Tests\AB.Extensions.Tests -c Release }
 
-exec { & dotnet pack .\src\MediatR -c Release -o .\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\src\AB.Extensions\AB.Extensions -c Release -o .\artifacts --version-suffix=$revision }
