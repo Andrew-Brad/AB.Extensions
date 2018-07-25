9eda524b87235113c662d7a8d237d50a722c6070
Andrew <zep519@yahoo.com>
2018-07-25 17:00:55 -0500

    console code improvement

--- test/CoreConsoleApp20/Program.cs
+++ test/CoreConsoleApp20/Program.cs
@@ -1,32 +1,38 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using static AB.Extensions.ConsoleExtensions;
 
 namespace CoreConsoleApp20
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            WriteLineWithColor("Printing in yellow.", ConsoleColor.Yellow);
-            WriteLineWithColor("Printing in green.", ConsoleColor.Green);
+            ClearCurrentConsoleLine(); // corner case
 
-            WriteLineWithColor("Cursor Left: " + Console.CursorLeft + ".  Cursor top:" + Console.CursorTop, ConsoleColor.White);
+            foreach (ConsoleColor color in Enum.GetValues(typeof(ConsoleColor)))
+            {
+                WriteLineWithColor($"Printing in {color.ToString()}.", color);
+            }
+
+            ClearConsoleLine(0); //black
+
+            WriteLineWithColor($"Cursor position: [{Console.CursorLeft},{Console.CursorTop}]", ConsoleColor.Green);
+            WriteLineWithColor("                                                         ", ConsoleColor.Black, ConsoleColor.DarkYellow);
             WriteToBottomLineWithColor("Setting time...", ConsoleColor.Red);
-            WriteToBottomLineWithColor("Corner case..." + new string('s',900000), ConsoleColor.Red);
-            ClearConsoleLine(1);
+            WriteToBottomLineWithColor("Corner case..." + new string('s', 900000), ConsoleColor.Red);
+            ClearCurrentConsoleLine();
 
             //Infinite task to print time at the bottom:
             Task.Run(async () =>
             {
                 while (true)
                 {
-                    await Task.Delay(2000);
+                    await Task.Delay(1000);
                     WriteToBottomLineWithColor(DateTime.Now.ToLongTimeString(), ConsoleColor.DarkRed);
                 }
             });
             Console.ReadKey();
         }
-
     }
 }
