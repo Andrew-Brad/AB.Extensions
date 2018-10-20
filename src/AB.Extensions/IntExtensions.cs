718d32d7d416c2203ccfd6537f57223888a85964
Andrew <zep519@yahoo.com>
2018-10-20 09:58:48 -0500

    some random implementation from SO to get benchmarking up

---
+++ src/AB.Extensions/IntExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AB.Extensions
+{
+    /// <summary>
+    /// Extensions for the <see cref="Int32"/> class.
+    /// </summary>
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Returns the first ordinal digit for a given <see cref="Int32"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        public static int FirstDigit(this int value)
+        {
+            if (value == Int32.MinValue) return 2;
+            int firstDigit = (int)(Math.Abs(value).ToString()[0]) - 48;
+            return firstDigit;
+        }
+
+        // todo:
+        // int64?
+        // extra methods for return types for micro digits? (first digit is always 1-9
+        // fix console extensions comments
+        // datetime tests - crush some tests into [Theory]
+    }
+}
---
+++ test/AB.Extensions.Tests/IntExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace AB.Extensions.Tests
+{
+    public class IntExtensionsTests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(987654321, 9)]
+        [InlineData(-10, 1)]
+        [InlineData(int.MaxValue, 2)]
+        [InlineData(-1, 1)]
+        [InlineData(0,0)]
+        [InlineData(int.MinValue, 2)]
+        [InlineData(1000000000, 1)]
+        public void Int_Get_First_Digit(int givenNumber, int expectedFirstDigit)
+        {
+            //Arrange
+
+            //Act
+            int firstDigit = givenNumber.FirstDigit();
+
+            //Assert
+            Assert.Equal(expectedFirstDigit, firstDigit);
+        }
+
+    }
+}
