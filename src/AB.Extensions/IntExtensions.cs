b50db8b25a30ce5e0c8a69bc35741ed70f10447d
Andrew <zep519@yahoo.com>
2018-10-25 21:07:35 -0500

    Integer extension: Return first digit with benchmark (#10)
    * some random implementation from SO to get benchmarking up
    
    * working benchmarks, passing test
    
    * split class into separate file

---
+++ src/AB.Extensions/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AB.Extensions
+{
+    /// <summary>
+    /// Extensions for the <see cref="Int32"/> class.
+    /// </summary>
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Returns the first ordinal digit for a given <see cref="Int32"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        public static int LeadingDigit(this int value)
+        {
+            if (value < 0 && (value = -value) < 0) return 2;
+            return (value < 100) ? (value < 1) ? 0 : (value < 10)
+                    ? value : value / 10 : (value < 1000000) ? (value < 10000)
+                    ? (value < 1000) ? value / 100 : value / 1000 : (value < 100000)
+                    ? value / 10000 : value / 100000 : (value < 100000000)
+                    ? (value < 10000000) ? value / 1000000 : value / 10000000
+                    : (value < 1000000000) ? value / 100000000 : value / 1000000000;
+        }
+    }
+}
